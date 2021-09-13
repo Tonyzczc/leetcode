e308a2cee6f926dbe2117e636ea9efa28c0a0d3c
zhangchi <718558450@qq.com>
2021-09-13 11:23:54 +0800

    二叉树中序遍历

---
+++ src/com/leetcode/BinaryTreeInorderTraversal.java
@@ -0,0 +1,57 @@
+package com.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://leetcode-cn.com/problems/binary-tree-inorder-traversal/
+ * 二叉树中序遍历
+ * @Author:Zhangchi
+ * @date:2021-09-13 10:54 上午
+ **/
+public class BinaryTreeInorderTraversal {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 平衡二叉树中序遍历
+     * 左-中-右
+     * @param root
+     * @return
+     */
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if(result == null){
+            return  result;
+        }
+        nodeIterator(root, result);
+
+        return result;
+    }
+
+    public static void nodeIterator(TreeNode root , List<Integer> result){
+        if(root == null){
+            return;
+        }
+        nodeIterator(root.left, result);
+        result.add(root.val);
+        nodeIterator(root.right,result);
+    }
+
+
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
