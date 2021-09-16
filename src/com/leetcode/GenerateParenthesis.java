7674cdae0d04663f75c1a91156ee6e98456a2eb0
zhangchi <718558450@qq.com>
2021-09-16 13:46:15 +0800

    括号生成

---
+++ src/com/leetcode/GenerateParenthesis.java
@@ -0,0 +1,70 @@
+package com.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author:Zhangchi
+ * @date:2021-09-16 12:50 下午
+ **/
+public class GenerateParenthesis {
+
+    public static void main(String[] args) {
+        GenerateParenthesis generateParenthesis = new GenerateParenthesis();
+        generateParenthesis.generateParenthesis(3);
+    }
+
+
+    public List<String> generateParenthesis(int n) {
+//        generate(0 , 2*n , "");
+        List<String> res = new ArrayList<>();
+        generateParenthe(0 , 0 , n , "" , res);
+        return res;
+    }
+
+    private void generate(int leven, int max, String str) {
+
+        if(leven >= max){
+            System.out.println(str);
+            return;
+        }
+
+        String s1 = str + "(";
+        String s2 = str + ")";
+
+        generate(leven+1, max, s1);
+        generate(leven+1, max, s2);
+    }
+
+    private void generateParenthe(int left , int right, int number, String str , List<String> result) {
+
+        if(left == number && right == number ){
+            result.add(str);
+            return;
+        }
+        if(left < number ) {
+            String s1 = str + "(";
+            generateParenthe(left + 1, right, number, s1, result);
+        }
+        //  (数量 > )数量
+        if(left > right) {
+            String s2 = str + ")";
+            generateParenthe(left, right + 1, number, s2, result);
+        }
+
+
+    }
+
+    public void addParenthesis(int left  , int right , List<String>  result){
+
+    }
+
+
+
+
+
+
+
+
+
+}
